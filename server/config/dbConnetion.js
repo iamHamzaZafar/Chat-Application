80625f2c3ca428f0f522f7f070907736cb5a58cf
Hamza Zafar <link2hamzazafar@gmail.com>
2024-11-17 21:07:22 +0500

    created the express server

--- server/config/dbConnetion.js
+++ server/config/dbConnetion.js
@@ -1,19 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const dotenv= require('dotenv')
+const dotenv = require("dotenv");
 dotenv.config();
 
+const connectToDb = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_DB_URI}/${process.env.DB_NAME}`
+    );
+  } catch (error) {
+    console.log("error", error.message);
+    process.exit(1);
+  }
+};
 
-const connectToDb = async () =>{
-
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${process.env.DB_NAME}`);
-    } catch (error) {
-        console.log("error" , error.message)
-        process.exit(1);
-    }
-}
-
-
-
-module.exports = {connectToDb}
+module.exports = { connectToDb };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { connectToDb } = require("./config/dbConnetion");
+const app = require('./app')
 
-
+const port = process.env.PORT || 3000
 
 // enviroment variables configrations
 dotenv.config();
@@ -11,3 +12,7 @@ dotenv.config();
 connectToDb().then(() => {
   console.log("Database connection successfull!!!");
 });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
