a5fca2ba7938af9c21070fbdf31818175624e351
Hamza Zafar <link2hamzazafar@gmail.com>
2024-11-19 20:43:27 +0500

    edit profile route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const { signup , verifyEmailController , login } = require("../controllers/auth.controller");
+const {
+  signup,
+  verifyEmailController,
+  login,
+} = require("../controllers/auth.controller");
+const { isAuthenticated } = require("../middlewares/isLoggedin");
 const router = express.Router();
 
 // route to create the user.
 router.post("/signup", signup);
 // route for the email verification.
-router.get('/verify-email', verifyEmailController);
+router.get("/verify-email", verifyEmailController);
 // route for the user login.
-router.post('/login' , login);
-
+router.post("/login", login);
+// route to edit the profile.
+router.put("/edit-profile/:id", isAuthenticated);
 
 module.exports = router;
